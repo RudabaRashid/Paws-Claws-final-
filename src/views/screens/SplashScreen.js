1d8af106e2331b0db9aa96abca335d69e08f4853
RudabaRashid <64989490+RudabaRashid@users.noreply.github.com>
2022-07-15 01:58:07 -0700

    first commit

---
+++ src/views/screens/SplashScreen.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Button, Text, View, } from 'react-native';
+
+
+import 'react-native-gesture-handler';
+import {SafeAreaView, TextInput, Image} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import COLORS from '../../const/colors';
+import {ScrollView, TouchableOpacity} from 'react-native-gesture-handler';
+
+
+
+const SplashScreen =({navigation}) =>{
+    return (
+
+        <SafeAreaView>
+            
+        <View>
+        <Text>SplashScreen</Text>
+        <Button title="SplashScreen" onPress={()=>navigation.navigate("SignIn")}></Button>
+        </View>
+
+        <Image
+                style={{width: 300, height: 240, alignSelf: 'center'}}
+                source={require('../../assets/vetsplash.jpg')}
+              />
+
+              
+              
+        </SafeAreaView>
+    
+    );
+};
+
+
+export default SplashScreen;
+
